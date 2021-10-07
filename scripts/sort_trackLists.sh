083c7c34f5689fa27517a8b5862b9d58c15359bc
childers <genetics.guy@gmail.com>
2021-10-07 16:18:06 -0400

    reorganizing to handle the scripts

---
+++ scripts/sort_trackLists.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+for filename in ./*/trackList.json; do
+#    for ((i=0; i<=3; i++)); do
+#        ./MyProgram.exe "$filename" "Logs/$(basename "$filename" .txt)_Log$i.txt"
+#    done
+    echo "processing $filename"
+    cat "$filename" |jq -S > "$filename".sorted 
+    mv "$filename" "filename".orig
+    mv "$filename".sorted "$filename" 
+done
